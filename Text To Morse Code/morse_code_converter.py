d07c296456722e1e26a0b756207128fa975c305d
Streetprophet90 <hamoafo@gmail.com>
2024-01-05 10:49:38 +0000

    morse_code_converter and readm.md  created

---
+++ Text To Morse Code/morse_code_converter.py
@@ -0,0 +1,30 @@
+# Morse Code Dictionary
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                  'C': '-.-.', 'D': '-..', 'E': '.',
+                  'F': '..-.', 'G': '--.', 'H': '....',
+                  'I': '..', 'J': '.---', 'K': '-.-',
+                  'L': '.-..', 'M': '--', 'N': '-.',
+                  'O': '---', 'P': '.--.', 'Q': '--.-',
+                  'R': '.-.', 'S': '...', 'T': '-',
+                  'U': '..-', 'V': '...-', 'W': '.--',
+                  'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                  '1': '.----', '2': '..---', '3': '...--',
+                  '4': '....-', '5': '.....', '6': '-....',
+                  '7': '--...', '8': '---..', '9': '----.',
+                  '0': '-----', ',': '--..--', '.': '.-.-.-',
+                  '?': '..--..', '/': '-..-.', '-': '-....-',
+                  '(': '-.--.', ')': '-.--.-', ' ': ' '}
+
+def text_to_morse(text):
+    morse_code = ''
+    for char in text.upper():
+        if char in MORSE_CODE_DICT:
+            morse_code += MORSE_CODE_DICT[char] + ' '
+        else:
+            morse_code += ' '
+    return morse_code
+
+if __name__ == "__main__":
+    user_input = input("Enter a string: ")
+    morse_result = text_to_morse(user_input)
+    print(f"Morse Code: {morse_result}")
