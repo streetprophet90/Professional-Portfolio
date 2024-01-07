b7a55b3ed8d12cc8ada61b1d25765cc06113697e
Streetprophet90 <hamoafo@gmail.com>
2024-01-07 20:31:25 +0000

    fxn created under class

--- 07 Dangerous Writing App (GUI)/dangerous_writing_app.py
+++ 07 Dangerous Writing App (GUI)/dangerous_writing_app.py
@@ -20,4 +20,19 @@ class DangerousWritingApp:
         self.monitor_thread = threading.Thread(target=self.monitor_inactivity)
         self.monitor_thread.start()
 
+    def create_widgets(self):
+        #Text Entry
+        self.text_entry = tk.Text(self.root, height=10, width=40)
+        self.text_entry.pack(padx=10, pady=10)
+
+        #start button
+        self.start_button = tk.Button(self.root, text="Start Writing", command=self.start_writing)
+        self.start_button(pady=10)
+
+    def start_writing(self):
+        self.text_entry.delete("1.0", tk.END) #Clear the text
+        self.last_update_time = time.time() #Reset timer
+
+    def monitor_inactivity(self):
         
+
