b92be59fc7baa1d2c8a1270b6e729beadb54e2d7
Streetprophet90 <hamoafo@gmail.com>
2024-01-09 16:40:29 +0000

    make soup

--- 10 Web Scraping/main.py
+++ 10 Web Scraping/main.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 import time
 import csv
 
+nba_url= "https://www.nba.com/stats/"
 chrome_driver_path = "C:\Development\chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver_path)
-driver.get("https://www.nba.com/stats/")
+driver.get(nba_url)
+
+soup = BeautifulSoup(nba_url, "html.parser")
