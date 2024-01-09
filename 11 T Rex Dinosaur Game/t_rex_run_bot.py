a6457b6db0a92cd440906836a1cc56b365649d0f
Streetprophet90 <hamoafo@gmail.com>
2024-01-09 20:38:18 +0000

    main function

--- 11 T Rex Dinosaur Game/t_rex_run_bot.py
+++ 11 T Rex Dinosaur Game/t_rex_run_bot.py
@@ -6,3 +6,7 @@ def is_obstacle(image):
         # Check if there is an obstacle on the screen based on pixel color
     obstacle_color = (83, 83, 83)  # Color of the obstacle in RGB
     return obstacle_color in image.getdata()
+
+def main():
+    print("Starting T-Rex Run Bot in 3 seconds...")
+    time.sleep(3)
