b843aa846272a8aa59377d8d7ab7723435e9e602
Streetprophet90 <hamoafo@gmail.com>
2024-01-09 20:37:23 +0000

    t_rex and fxn created

---
+++ 11 T Rex Dinosaur Game/t_rex_run_bot.py
@@ -0,0 +1,8 @@
+import time
+import PIL import ImageGrab
+import pyautogui
+
+def is_obstacle(image):
+        # Check if there is an obstacle on the screen based on pixel color
+    obstacle_color = (83, 83, 83)  # Color of the obstacle in RGB
+    return obstacle_color in image.getdata()
