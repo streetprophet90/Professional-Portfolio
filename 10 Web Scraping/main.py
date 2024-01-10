f4769657e767a754f2f18b1c2e776a10091e725a
Streetprophet90 <hamoafo@gmail.com>
2024-01-10 11:07:36 +0000

    getText() set

--- 10 Web Scraping/main.py
+++ 10 Web Scraping/main.py
@@ -18,7 +18,17 @@ nba_page = response.text
 soup = BeautifulSoup(nba_page, "xml")
 nba_tag = soup.find(name="a", class_="Anchor_anchor__cSc3P LeaderBoardPlayerCard_lbpcTableLink__MDNgL")
 stats_heading = soup.find(name="h2", class_="LeaderBoardCard_lbcTitle___WI9J")
+nba_player = nba_tag.getText()
 stats_text = stats_heading.getText()
+player_link = nba_tag.get("href")
+player_team = soup.find(name="span", class_="LeaderBoardPlayerCard_lbpcTeamAbbr__fGlx3").getText()
+print(nba_player)
 print(stats_text)
+print(player_link)
+print(player_team)
+
+
+
+
 
 
