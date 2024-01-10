954515baf622128c61331f2f03c107dd342b1872
Streetprophet90 <hamoafo@gmail.com>
2024-01-10 10:25:21 +0000

    soup issue fixed

--- 10 Web Scraping/main.py
+++ 10 Web Scraping/main.py
@@ -4,9 +4,19 @@ from selenium import webdriver
 import time
 import csv
 
-nba_url= "https://www.nba.com/stats/"
-chrome_driver_path = "C:\Development\chromedriver.exe"
-driver = webdriver.Chrome(chrome_driver_path)
-driver.get(nba_url)
 
-soup = BeautifulSoup(nba_url, "html.parser")
+# chrome_driver_path = "C:\Development\chromedriver.exe"
+# driver = webdriver.Chrome(chrome_driver_path)
+# driver.get(nba_url)
+#
+# soup = BeautifulSoup(nba_url, "html.parser")
+
+
+nba_url = "https://www.nba.com/stats/"
+response = requests.get(nba_url)
+nba_page = response.text
+soup = BeautifulSoup(nba_page, "xml")
+print(soup.title)
+nba_tag = soup.find(name="a", class_="Anchor_anchor__cSc3P LeaderBoardPlayerCard_lbpcTableLink__MDNgL")
+print(nba_tag)
+
