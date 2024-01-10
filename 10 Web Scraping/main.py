4b10639b23a8831b1569c9168a8dd7f85fb34555
Streetprophet90 <hamoafo@gmail.com>
2024-01-10 11:40:24 +0000

    for loop to scrape

--- 10 Web Scraping/main.py
+++ 10 Web Scraping/main.py
@@ -16,16 +16,28 @@ nba_url = "https://www.nba.com/stats/"
 response = requests.get(nba_url)
 nba_page = response.text
 soup = BeautifulSoup(nba_page, "xml")
-nba_tag = soup.find(name="a", class_="Anchor_anchor__cSc3P LeaderBoardPlayerCard_lbpcTableLink__MDNgL")
-stats_heading = soup.find(name="h2", class_="LeaderBoardCard_lbcTitle___WI9J")
-nba_player = nba_tag.getText()
-stats_text = stats_heading.getText()
-player_link = nba_tag.get("href")
-player_team = soup.find(name="span", class_="LeaderBoardPlayerCard_lbpcTeamAbbr__fGlx3").getText()
-print(nba_player)
-print(stats_text)
-print(player_link)
-print(player_team)
+nba = soup.find_all(name="a", class_="Anchor_anchor__cSc3P LeaderBoardPlayerCard_lbpcTableLink__MDNgL")
+stats_heading = soup.find_all(name="h2", class_="LeaderBoardCard_lbcTitle___WI9J")
+
+
+player_name = []
+player_links = []
+player_team = []
+
+for nba_tag in nba:
+    nba_player = nba_tag.getText()
+    player_name.append(nba_player)
+
+    link = nba_tag.get("href")
+    player_links.append(link)
+
+team = [player_team.getText() for player_team in soup.find_all(name="span", class_="LeaderBoardPlayerCard_lbpcTeamAbbr__fGlx3")]
+
+
+print(player_name)
+print(player_links)
+print(team)
+
 
 
 
