5b330462e4a093a8fe952d4504652d80ab161aec
Streetprophet90 <hamoafo@gmail.com>
2024-01-11 14:17:26 +0000

    date stripped

--- 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
+++ 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
@@ -8,6 +8,12 @@ def find_jobs():
     soup = BeautifulSoup(html_text, 'lxml')
     jobs = soup.find_all("div", class_="hp-grid__item hp-col-sm-6 hp-col-xs-12")
 
+    # csv_headers = ['Company', 'Job Category', 'Date', 'More Info']
+    # with open('results.csv', 'w', newline='', encoding='utf-8') as file:
+    #     csv_writer = csv.writer(file)
+    #     csv_writer.writerow(csv_headers)
+
+
     for job in jobs:
         date_added = job.find('time', class_="hp-listing__created-date hp-listing__date hp-meta").text.replace(',', '')
         company_name = job.find('h4', class_='hp-listing__title').text
@@ -15,9 +21,11 @@ def find_jobs():
         more_info = job.header.a['href']
 
 
+
+
         print(f"Company Name: {company_name.strip()}")
         print(f"Job Category: {category.strip()}")
-        print(f"Date: {date_added.strip()}")
+        print(f"Date: {date_added.strip()[9:]}")
         print(f'More info: {more_info}')
         print('')
 
