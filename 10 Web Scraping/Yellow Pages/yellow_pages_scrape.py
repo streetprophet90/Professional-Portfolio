b4f7806d0d1f5a96534f9c30df9a13c161eb2bca
Streetprophet90 <hamoafo@gmail.com>
2024-01-11 13:20:13 +0000

    for loop and white spaces stripped

--- 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
+++ 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
@@ -8,7 +8,13 @@ soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all("div", class_="hp-grid__item hp-col-sm-6 hp-col-xs-12")
 
 for job in jobs:
-    company_name = job.find('h4', class_='hp-listing__title')
-    location = job.find('i', class_="hp-icon fas fa-map-marker-alt")
-    category = job.find('div', class_="hp-listing__categories hp-listing__category")
-    date_added = job.find('time', class_="hp-listing__created-date hp-listing__date hp-meta")
+    company_name = job.find('h4', class_='hp-listing__title').text
+    category = job.find('div', class_="hp-listing__categories hp-listing__category").text
+    date_added = job.find('time', class_="hp-listing__created-date hp-listing__date hp-meta").text.replace(',','')
+
+    print(f"Company Name: {company_name.strip()}")
+    print(f"Job Category: {category.strip()}")
+    print(f"Date: {date_added.strip()}")
+    print('')
+
+
