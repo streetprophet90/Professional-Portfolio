719959ba451287b39fa265f267bc3f86c104676b
Streetprophet90 <hamoafo@gmail.com>
2024-01-11 13:45:13 +0000

    more info links set

--- 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
+++ 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
@@ -8,13 +8,16 @@ soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all("div", class_="hp-grid__item hp-col-sm-6 hp-col-xs-12")
 
 for job in jobs:
+    date_added = job.find('time', class_="hp-listing__created-date hp-listing__date hp-meta").text.replace(',', '')
     company_name = job.find('h4', class_='hp-listing__title').text
     category = job.find('div', class_="hp-listing__categories hp-listing__category").text
-    date_added = job.find('time', class_="hp-listing__created-date hp-listing__date hp-meta").text.replace(',','')
+    more_info = job.header.a['href']
+
 
     print(f"Company Name: {company_name.strip()}")
     print(f"Job Category: {category.strip()}")
     print(f"Date: {date_added.strip()}")
+    print(f'More info: {more_info}')
     print('')
 
 
