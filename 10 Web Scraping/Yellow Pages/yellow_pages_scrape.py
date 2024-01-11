c6672c46bf9bc55a443e82cf98034054d512dea5
Streetprophet90 <hamoafo@gmail.com>
2024-01-11 13:55:28 +0000

    fxn created and __name__ set

--- 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
+++ 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
@@ -3,21 +3,30 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-html_text = requests.get("https://ghanayellowpages.com/listings/").text
-soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all("div", class_="hp-grid__item hp-col-sm-6 hp-col-xs-12")
+def find_jobs():
+    html_text = requests.get("https://ghanayellowpages.com/listings/").text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all("div", class_="hp-grid__item hp-col-sm-6 hp-col-xs-12")
 
-for job in jobs:
-    date_added = job.find('time', class_="hp-listing__created-date hp-listing__date hp-meta").text.replace(',', '')
-    company_name = job.find('h4', class_='hp-listing__title').text
-    category = job.find('div', class_="hp-listing__categories hp-listing__category").text
-    more_info = job.header.a['href']
+    for job in jobs:
+        date_added = job.find('time', class_="hp-listing__created-date hp-listing__date hp-meta").text.replace(',', '')
+        company_name = job.find('h4', class_='hp-listing__title').text
+        category = job.find('div', class_="hp-listing__categories hp-listing__category").text
+        more_info = job.header.a['href']
 
 
-    print(f"Company Name: {company_name.strip()}")
-    print(f"Job Category: {category.strip()}")
-    print(f"Date: {date_added.strip()}")
-    print(f'More info: {more_info}')
-    print('')
+        print(f"Company Name: {company_name.strip()}")
+        print(f"Job Category: {category.strip()}")
+        print(f"Date: {date_added.strip()}")
+        print(f'More info: {more_info}')
+        print('')
+
+if __name__ == "__main__":
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes... ')
+        time.sleep(time_wait * 60)
+
 
 
