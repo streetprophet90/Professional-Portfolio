34fc30c5347175b8668238dcdc0cbde9d4586de2
Streetprophet90 <hamoafo@gmail.com>
2024-01-11 12:56:27 +0000

    beautiful soup made

--- 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
+++ 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
@@ -5,6 +5,6 @@ from bs4 import BeautifulSoup
 
 html_text = requests.get("https://ghanayellowpages.com/listings/").text
 soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all("div", "hp-listings hp-block hp-grid")
+jobs = soup.find_all("div", class_="hp-grid__item hp-col-sm-6 hp-col-xs-12")
 
 print(jobs)
