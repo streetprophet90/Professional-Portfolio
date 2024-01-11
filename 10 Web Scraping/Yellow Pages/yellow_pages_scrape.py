7502e84babaf88487f1ed0acd9e6ea9a66a32554
Streetprophet90 <hamoafo@gmail.com>
2024-01-11 13:07:37 +0000

    company name,location category, date scraped

--- 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
+++ 10 Web Scraping/Yellow Pages/yellow_pages_scrape.py
@@ -7,4 +7,8 @@ html_text = requests.get("https://ghanayellowpages.com/listings/").text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all("div", class_="hp-grid__item hp-col-sm-6 hp-col-xs-12")
 
-print(jobs)
+for job in jobs:
+    company_name = job.find('h4', class_='hp-listing__title')
+    location = job.find('i', class_="hp-icon fas fa-map-marker-alt")
+    category = job.find('div', class_="hp-listing__categories hp-listing__category")
+    date_added = job.find('time', class_="hp-listing__created-date hp-listing__date hp-meta")
