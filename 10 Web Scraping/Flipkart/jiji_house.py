579d433760df6a8551c66046beb58fd600d1536d
Streetprophet90 <hamoafo@gmail.com>
2024-01-12 10:43:46 +0000

    whitespaces fixed

--- 10 Web Scraping/Flipkart/jiji_house.py
+++ 10 Web Scraping/Flipkart/jiji_house.py
@@ -27,27 +27,27 @@ print(response)
 House = []
 Prices = []
 Desc = []
-Location = []
+# Location = []
 
 soup = BeautifulSoup(response.text, "lxml")
 names = soup.find_all("div", "b-list-advert-base__data")
 
 for name in names:
-    house = name.find("div", class_="b-advert-title-inner qa-advert-title b-advert-title-inner--div").text
+    house = name.find("div", class_="b-advert-title-inner qa-advert-title b-advert-title-inner--div").text.strip()
     House.append(house)
 
-    prices = name.find("div", class_="qa-advert-price").text
+    prices = name.find("div", class_="qa-advert-price").text.strip()
     Prices.append(prices)
 
-    location = name.find('span', class_="b-list-advert__region__text")
-    Location.append(location)
+    # location = name.find(name='span', class_="b-list-advert__region__text")
+    # Location.append(location)
 
-    description = name.find("div", class_="b-list-advert-base__description-text").text
+    description = name.find("div", class_="b-list-advert-base__description-text").text.strip()
     Desc.append(description)
 
 print(House)
 print(Prices)
-print(Location)
+# print(Location)
 print(Desc)
 
 
