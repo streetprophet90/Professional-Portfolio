8e5c62d2a44aad066cc89dac556c8ccdfee372cb
Streetprophet90 <hamoafo@gmail.com>
2024-01-12 11:07:52 +0000

    tags removed

--- 10 Web Scraping/Flipkart/jiji_house.py
+++ 10 Web Scraping/Flipkart/jiji_house.py
@@ -3,23 +3,6 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-# html_text = ("https://www.flipkart.com/search?q=phones&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off")
-# response = requests.get(html_text)
-# print(response)
-#
-# Names = []
-# Prices = []
-# Desc = []
-# Reviews = []
-#
-# soup = BeautifulSoup(response.text, "lxml")
-# names = soup.find_all("div", "_4rR01T")
-#
-# for name in names:
-#     phone_name = name.text
-#     Names.append(phone_name)
-# print(Names)
-
 html_text = ("https://jiji.com.gh/new-builds")
 response = requests.get(html_text)
 print(response)
