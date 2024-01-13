0899c50acd3e0f5a511ad5d2f56cebf34d587005
Streetprophet90 <hamoafo@gmail.com>
2024-01-13 06:22:39 +0000

    while loop set

--- 12 Selenium/Cookie Game/cookie_playing_bot.py
+++ 12 Selenium/Cookie Game/cookie_playing_bot.py
@@ -6,3 +6,23 @@ chrome_driver_path = "YOUR CHROME DRIVER PATH"
 driver = webdriver.Chrome(chrome_driver_path)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
+# Get cookie to click on.
+cookie = driver.find_element(By.ID, "cookie")
+
+# Get upgrade item ids.
+items = driver.find_elements(By.CSS_SELECTOR, "#store div")
+item_ids = [item.get_attribute("id") for item in items]
+
+timeout = time.time() + 5
+five_min = time.time() + 60*5  # 5 minutes
+
+while True:
+    cookie.click()
+
+    # Every 5 seconds:
+    if time.time() > timeout:
+
+        # Get all upgrade <b> tags
+        all_prices = driver.find_elements(By.CSS_SELECTOR, "#store b")
+        item_prices = []
+
