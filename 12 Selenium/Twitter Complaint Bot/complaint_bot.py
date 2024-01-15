cb4c7a30a974d537bd1b9f7dc60d18689eb47bdb
Streetprophet90 <hamoafo@gmail.com>
2024-01-15 16:28:04 +0000

    .py file created

---
+++ 12 Selenium/Twitter Complaint Bot/complaint_bot.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+CHROME_DRIVER_PATH = 'YOUR CHROME DRIVER PATH'
+TWITTER_EMAIL = 'YOUR TWITTER EMAIL'
+TWITTER_PASSWORD = 'YOUR TWITTER PASSWORD'
+
+
