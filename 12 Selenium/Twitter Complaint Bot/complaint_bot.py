c265ac7e4b60e2ee1b42f1143d9ad6df6c3d60ef
Streetprophet90 <hamoafo@gmail.com>
2024-01-15 16:28:45 +0000

    class InternetSpeedTwitterBOt created

--- 12 Selenium/Twitter Complaint Bot/complaint_bot.py
+++ 12 Selenium/Twitter Complaint Bot/complaint_bot.py
@@ -8,4 +8,9 @@ CHROME_DRIVER_PATH = 'YOUR CHROME DRIVER PATH'
 TWITTER_EMAIL = 'YOUR TWITTER EMAIL'
 TWITTER_PASSWORD = 'YOUR TWITTER PASSWORD'
 
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.up = 0
+        self.down = 0
 
