23e7829a51b9a32be9666277a12fe3f091e98ccb
Streetprophet90 <hamoafo@gmail.com>
2024-01-16 18:46:33 +0000

    searching selenium in yahoo search

--- 12 Selenium/Tech With Tim/main.py
+++ 12 Selenium/Tech With Tim/main.py
@@ -6,12 +6,19 @@ import time
 
 
 driver = webdriver.Chrome()
-driver .get("https://www.techwithtim.net/")
-print(driver.title)
+# driver .get("https://www.techwithtim.net/")
+# print(driver.title)
+#
+# element = driver.find_element(By.TAG_NAME , 'div')
+# print(element)
+# print(driver.page_source)
 
-element = driver.find_element(By.TAG_NAME , 'div')
-print(element)
-print(driver.page_source)
+driver.get('http://www.yahoo.com')
+assert 'Yahoo' in driver.title
+
+elem = driver.find_element(By.NAME, 'p')  # Find the search box
+elem.send_keys('seleniumhq' + Keys.RETURN)
+
 
 
 
