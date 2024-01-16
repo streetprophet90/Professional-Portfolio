3411ecb32a9f389088384f0bde1b198c856a9eb4
Streetprophet90 <hamoafo@gmail.com>
2024-01-16 19:42:22 +0000

    fixing clickable interruption

--- 12 Selenium/Tech With Tim/main.py
+++ 12 Selenium/Tech With Tim/main.py
@@ -1,18 +1,36 @@
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-driver = webdriver.Chrome()
-# driver .get("https://www.techwithtim.net/")
-# print(driver.title)
-#
-# element = driver.find_element(By.TAG_NAME , 'div')
-# print(element)
-# print(driver.page_source)
+# driver = webdriver.Chrome()
+# # driver .get("https://www.techwithtim.net/")
+# # print(driver.title)
+# #
+# # element = driver.find_element(By.TAG_NAME , 'div')
+# # # print(element)
+# # # print(driver.page_source)
+# #
+# #
+# # link = driver.find_element(By.LINK_TEXT, "Tutorials")
+# # link.click()
+# #
+# # time.sleep(5)
+# #
+# #
+# # try:
+# #     element = WebDriverWait(driver, 10).until(
+# #         EC.presence_of_element_located((By.LINK_TEXT, "From basic to advanced. Learn python programming."))
+# #     )
+# # finally:
+# #     time.sleep(300)
 
+driver = webdriver.Chrome()
 driver.get('http://www.yahoo.com')
 assert 'Yahoo' in driver.title
 
@@ -20,10 +38,15 @@ elem = driver.find_element(By.NAME, 'p')  # Find the search box
 elem.send_keys('seleniumhq' + Keys.RETURN)
 print(f"Successful: {elem}")
 
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Getting Started · Selenium IDE"))
+    )
+finally:
+    time.sleep(5)
 
 
-
-time.sleep(300)
+# time.sleep(300)
 
 
 
