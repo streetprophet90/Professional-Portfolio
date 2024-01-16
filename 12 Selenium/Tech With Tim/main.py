989cbb25b54ebbcc29fcd69f9f4fefbe0d11748b
Streetprophet90 <hamoafo@gmail.com>
2024-01-16 18:23:33 +0000

    page source

--- 12 Selenium/Tech With Tim/main.py
+++ 12 Selenium/Tech With Tim/main.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import time
 
 
 driver = webdriver.Chrome()
 driver .get("https://www.techwithtim.net/")
 print(driver.title)
+
+element = driver.find_element(By.TAG_NAME , 'div')
+print(element)
+print(driver.page_source)
+
+
 
 time.sleep(300)
 
