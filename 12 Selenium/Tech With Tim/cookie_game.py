5bc59d4d30ec623219df59eada549baa99de908e
Streetprophet90 <hamoafo@gmail.com>
2024-01-17 11:43:59 +0000

    got IDs from website and set webdriver chrome

--- 12 Selenium/Tech With Tim/cookie_game.py
+++ 12 Selenium/Tech With Tim/cookie_game.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -7,5 +8,14 @@ driver = webdriver.Chrome()
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 driver.maximize_window()
 print(driver)
+
+driver.implicitly_wait(5)
+
+language = driver.find_element(By.ID, "langSelect-EN")
+cookie = driver.find_element(By.ID, "bigCookie")
+cookie_count = driver.find_element(By.ID, "cookies")
+
+actions = ActionChains(driver)
+
 
 time.sleep(300)
