7ec1487ffafc2c5040ff2bcbfb2d33b54df9cba2
Streetprophet90 <hamoafo@gmail.com>
2024-01-17 09:07:53 +0000

    maximise.window fix clickable error

--- 12 Selenium/Tech With Tim/main.py
+++ 12 Selenium/Tech With Tim/main.py
@@ -8,45 +8,46 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-# driver = webdriver.Chrome()
-# # driver .get("https://www.techwithtim.net/")
-# # print(driver.title)
-# #
-# # element = driver.find_element(By.TAG_NAME , 'div')
-# # # print(element)
-# # # print(driver.page_source)
-# #
-# #
-# # link = driver.find_element(By.LINK_TEXT, "Tutorials")
-# # link.click()
-# #
-# # time.sleep(5)
-# #
-# #
-# # try:
-# #     element = WebDriverWait(driver, 10).until(
-# #         EC.presence_of_element_located((By.LINK_TEXT, "From basic to advanced. Learn python programming."))
-# #     )
-# # finally:
-# #     time.sleep(300)
-
 driver = webdriver.Chrome()
-driver.get('http://www.yahoo.com')
-assert 'Yahoo' in driver.title
+driver .get("https://www.techwithtim.net/")
+driver.maximize_window()
+print(driver.title)
 
-elem = driver.find_element(By.NAME, 'p')  # Find the search box
-elem.send_keys('seleniumhq' + Keys.RETURN)
-print(f"Successful: {elem}")
+# element = driver.find_element(By.TAG_NAME , 'div')
+# print(element)
+# print(driver.page_source)
+
+
+link = driver.find_element(By.LINK_TEXT, "Tutorials")
+link.click()
+
+# time.sleep(5)
+
 
 try:
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.LINK_TEXT, "Getting Started · Selenium IDE"))
+        EC.presence_of_element_located((By.LINK_TEXT, "From basic to advanced. Learn python programming."))
     )
 finally:
-    time.sleep(5)
+    time.sleep(300)
+
+# driver = webdriver.Chrome()
+# driver.get('http://www.yahoo.com')
+# assert 'Yahoo' in driver.title
+#
+# elem = driver.find_element(By.NAME, 'p')  # Find the search box
+# elem.send_keys('seleniumhq' + Keys.RETURN)
+# print(f"Successful: {elem}")
+#
+# try:
+#     element = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.LINK_TEXT, "Getting Started · Selenium IDE"))
+#     )
+# finally:
+#     time.sleep(5)
 
 
-# time.sleep(300)
+time.sleep(300)
 
 
 
