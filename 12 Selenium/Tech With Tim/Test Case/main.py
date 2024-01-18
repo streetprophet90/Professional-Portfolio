a8eb7c6f4d6e9bcb9961f736ba269384ec51a1b5
Streetprophet90 <hamoafo@gmail.com>
2024-01-18 13:57:54 +0000

    SetUp fxn created

--- 12 Selenium/Tech With Tim/Test Case/main.py
+++ 12 Selenium/Tech With Tim/Test Case/main.py
@@ -3,3 +3,9 @@ from selenium import webdriver
 import page
 
 class PythonOrgSearch(unittest.TestCase):
+
+    def SetUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://www.python.org")
+
+
