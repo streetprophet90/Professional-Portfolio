b9e8bd897af68964197b45c1e7645a6027a16fe1
Streetprophet90 <hamoafo@gmail.com>
2024-01-18 14:41:36 +0000

    comments from page.py

--- 12 Selenium/Tech With Tim/Test Case/locator.py
+++ 12 Selenium/Tech With Tim/Test Case/locator.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators(object):
-    GO_BUTTON = (By.ID, "submit")
+    GO_BUTTON = (By.ID, "submit")  #1_this is the code
 
 class SearchResultsPageLocators(object):
     pass
