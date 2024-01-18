d1ead8bf338454fbf0afc354549d623a4d4dc1f3
Streetprophet90 <hamoafo@gmail.com>
2024-01-18 14:20:07 +0000

    fxn click_go_button created

--- 12 Selenium/Tech With Tim/Test Case/page.py
+++ 12 Selenium/Tech With Tim/Test Case/page.py
@@ -6,3 +6,7 @@ class MainPage(BasePage):
 
     def is_title_matches(self):
         return "Python" in self.driver.title
+
+    def click_go_button(self):
+        element = self.driver.find_element()
+        element.click()
