f47f09e352686963879393f55da3901655f7b31a
Streetprophet90 <hamoafo@gmail.com>
2024-01-20 12:07:42 +0000

    GymClassBooker class set

--- 12 Selenium/Gym Class Bookings/main.py
+++ 12 Selenium/Gym Class Bookings/main.py
@@ -4,3 +4,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+class GymClassBooker:
+    def __init__(self, gym_url, username, password):
+        self.gym_url = gym_url
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome()
+
