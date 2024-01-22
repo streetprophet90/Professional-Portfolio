d0de149ea5e49f9f187fab07423778e677fb2a65
Streetprophet90 <hamoafo@gmail.com>
2024-01-22 13:44:51 +0000

    class libraryBookRenewal defined

--- 12 Selenium/Library Book Renewals/main.py
+++ 12 Selenium/Library Book Renewals/main.py
@@ -3,3 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+class LibraryBookRenewal:
+    def __init__(self, library_url, username, password):
+        self.library_url = library_url
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome()
+
