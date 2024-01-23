6d0ed00eb26ca1e64c9292598a058a0efb1f47bd
Streetprophet90 <hamoafo@gmail.com>
2024-01-23 14:47:17 +0000

    comments on instructions

--- 00 Leetcode Practice/rock_paperscissor.py
+++ 00 Leetcode Practice/rock_paperscissor.py
@@ -1,3 +1,12 @@
+# Rock Paper Scissors
+# Let's play! You have to return which player won! In case of a draw return Draw!.
+#
+# Examples(Input1, Input2 --> Output):
+#
+# "scissors", "paper" --> "Player 1 won!"
+# "scissors", "rock" --> "Player 2 won!"
+# "paper", "paper" --> "Draw!"
+
 def rps(p1, p2):
     # Define the winning combinations
     winning_combinations = {
