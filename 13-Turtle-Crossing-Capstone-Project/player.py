5203400ce55c2407ae0124591ee0b90d48f7a9ea
Streetprophet90 <hamoafo@gmail.com>
2024-01-24 14:18:39 +0000

    initalise class Player(turtle)

--- 13-Turtle-Crossing-Capstone-Project/player.py
+++ 13-Turtle-Crossing-Capstone-Project/player.py
@@ -3,3 +3,12 @@ from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('Turtle')
+        self.penup()
+        self.go_to_start()
+        self.setheading(90)
