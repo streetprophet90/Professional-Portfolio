ce218a9693979c47562282963ca564ec4372767a
Streetprophet90 <hamoafo@gmail.com>
2024-01-24 14:44:13 +0000

    scoreboard imported

--- 13-Turtle-Crossing-Capstone-Project/main.py
+++ 13-Turtle-Crossing-Capstone-Project/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -8,6 +9,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+score_board = Scoreboard()
 
 screen.listen()
 
