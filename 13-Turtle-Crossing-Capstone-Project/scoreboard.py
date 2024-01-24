5576b47a2a073fdc3b0fd278e8d8e9e223b463ef
Streetprophet90 <hamoafo@gmail.com>
2024-01-24 14:43:08 +0000

    update scoreboard, increase level, game over, fxns defined

--- 13-Turtle-Crossing-Capstone-Project/scoreboard.py
+++ 13-Turtle-Crossing-Capstone-Project/scoreboard.py
@@ -11,4 +11,12 @@ class Scoreboard(Turtle):
         self.goto(-250, 250)
         self.update_scoreboard()
 
-
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
