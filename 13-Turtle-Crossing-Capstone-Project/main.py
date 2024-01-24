49699970e10f786725ad5c57590569f0302f5ad3
Streetprophet90 <hamoafo@gmail.com>
2024-01-24 14:51:40 +0000

    fix key movement error

--- 13-Turtle-Crossing-Capstone-Project/main.py
+++ 13-Turtle-Crossing-Capstone-Project/main.py
@@ -2,6 +2,7 @@ from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -12,6 +13,27 @@ car_manager = CarManager()
 score_board = Scoreboard()
 
 screen.listen()
+screen.onkey(player.go_up, 'Up')
 
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            score_board.game_over()
+
+    #detectt a successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        score_board.increase_level()
 
 screen.exitonclick()
