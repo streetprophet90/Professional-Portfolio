c7fa108f66ecb52311be8e11f99efc214cfa3cb8
Streetprophet90 <hamoafo@gmail.com>
2024-01-24 14:22:30 +0000

    finish line

--- 13-Turtle-Crossing-Capstone-Project/player.py
+++ 13-Turtle-Crossing-Capstone-Project/player.py
@@ -18,3 +18,9 @@ class Player(Turtle):
 
     def go_to_start(self):
         self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
