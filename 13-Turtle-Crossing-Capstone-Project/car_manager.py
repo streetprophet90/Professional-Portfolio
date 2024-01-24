74a7a2397b07fa04060e342c4c1691d1dd75d0f7
Streetprophet90 <hamoafo@gmail.com>
2024-01-24 14:56:10 +0000

    yellow car fixed

--- 13-Turtle-Crossing-Capstone-Project/car_manager.py
+++ 13-Turtle-Crossing-Capstone-Project/car_manager.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 import random
 
-COLORS = ['red', 'orange', 'yelow', 'green', 'blue', 'purple']
+COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
