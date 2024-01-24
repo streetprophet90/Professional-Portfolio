e1ff14a7cd6a1604ee253efb09b1ed1a9f89e684
Streetprophet90 <hamoafo@gmail.com>
2024-01-24 14:35:10 +0000

    import CarManager

--- 13-Turtle-Crossing-Capstone-Project/main.py
+++ 13-Turtle-Crossing-Capstone-Project/main.py
@@ -1,11 +1,13 @@
 from turtle import Screen, Turtle
 from player import Player
+from car_manager import CarManager
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 
