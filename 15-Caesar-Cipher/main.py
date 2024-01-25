8d206e2b97fd0ee165c8fe7cb60aa440bdaad816
Streetprophet90 <hamoafo@gmail.com>
2024-01-25 16:26:22 +0000

    shift numbber

--- 15-Caesar-Cipher/main.py
+++ 15-Caesar-Cipher/main.py
@@ -31,3 +31,16 @@ while not should_end:
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
   text = input("Type your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
+
+  #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+  #Try running the program and entering a shift number of 45.
+  #Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+  #Hint: Think about how you can use the modulus (%).
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
