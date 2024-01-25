3355dbe1a8ce257ca60f50c4de7279dd20b404d2
Streetprophet90 <hamoafo@gmail.com>
2024-01-25 15:57:49 +0000

    check if user has got all letters

--- 14-Hangman/main.py
+++ 14-Hangman/main.py
@@ -36,3 +36,12 @@ while not end_of_game:
         if lives == 0:
             end_of_game = True
             print("You lose.")
+
+ #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
