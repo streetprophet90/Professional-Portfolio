087a8cf1b16953a97c67c378145bb6de6364ea38
Streetprophet90 <hamoafo@gmail.com>
2024-01-25 15:51:53 +0000

    for loop for display

--- 14-Hangman/main.py
+++ 14-Hangman/main.py
@@ -6,4 +6,12 @@ chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
-lives = 5
+lives = 6
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
