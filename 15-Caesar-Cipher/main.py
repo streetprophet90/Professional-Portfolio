48bd1a5f2ac3b8f46cf7b3617c30aeb858dc025d
Streetprophet90 <hamoafo@gmail.com>
2024-01-25 16:25:18 +0000

    finding a way for user if the want to restart the program

--- 15-Caesar-Cipher/main.py
+++ 15-Caesar-Cipher/main.py
@@ -17,3 +17,12 @@ def caesar(start_text, shift_amount, cipher_direction):
       end_text += char
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
+# #TODO-1: Import and print the logo from art.py when the program starts.
+# from art import logo
+# print(logo)
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+should_end = False
