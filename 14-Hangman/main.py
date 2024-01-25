d878972d260825696a1fa6afcbba3085bac7c601
Streetprophet90 <hamoafo@gmail.com>
2024-01-25 15:56:56 +0000

    check if user is wrong

--- 14-Hangman/main.py
+++ 14-Hangman/main.py
@@ -26,3 +26,13 @@ while not end_of_game:
         #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
+
+    # Check if user is wrong.
+    if guess not in chosen_word:
+        # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
