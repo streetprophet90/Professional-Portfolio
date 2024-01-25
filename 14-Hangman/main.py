e827ff820da91d0e6be3ef3f1020a2a9691f489e
Streetprophet90 <hamoafo@gmail.com>
2024-01-25 15:27:28 +0000

    variable chosen_word and end_of_game

--- 14-Hangman/main.py
+++ 14-Hangman/main.py
@@ -2,3 +2,8 @@ from hangman_words import word_list
 from hangman_art import stages, logo
 import random
 
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 5
