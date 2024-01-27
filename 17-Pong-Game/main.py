9ee6a43b0f51966296ae4b3ef3bbc9937f812ccb
Streetprophet90 <hamoafo@gmail.com>
2024-01-27 15:04:42 +0000

    detect collisions with, wall, paddle and detect if paddle misses

--- 17-Pong-Game/main.py
+++ 17-Pong-Game/main.py
@@ -21,3 +21,27 @@ screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    ball.move()
+
+    #Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Detect L paddle misses:
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
