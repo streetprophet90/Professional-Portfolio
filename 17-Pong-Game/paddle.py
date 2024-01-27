6526660ebc44774d2651676f01a38ae3fd381460
Streetprophet90 <hamoafo@gmail.com>
2024-01-27 14:49:12 +0000

    go_up fxn defined

--- 17-Pong-Game/paddle.py
+++ 17-Pong-Game/paddle.py
@@ -10,3 +10,8 @@ class Paddle(Turtle):
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
