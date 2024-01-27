6963e689aa9c49ae8607fea68c040c14f17a96cd
Streetprophet90 <hamoafo@gmail.com>
2024-01-27 14:46:59 +0000

    setting screen

---
+++ 17-Pong-Game/main.py
@@ -0,0 +1,11 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
