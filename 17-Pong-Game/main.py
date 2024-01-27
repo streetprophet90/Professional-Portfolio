bbe0cc193a90f7ed5723db86b369d0ae581b8423
Streetprophet90 <hamoafo@gmail.com>
2024-01-27 15:02:06 +0000

    screen.onkey, paddle, ball and scoreboard imported

--- 17-Pong-Game/main.py
+++ 17-Pong-Game/main.py
@@ -9,3 +9,15 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
