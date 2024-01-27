8816a12e6878e88f7ea1f47a169f347b2b40edc0
Streetprophet90 <hamoafo@gmail.com>
2024-01-27 14:53:33 +0000

    move fxn defined

--- 17-Pong-Game/ball.py
+++ 17-Pong-Game/ball.py
@@ -10,3 +10,8 @@ class Ball(Turtle):
         self.x_move = 3
         self.y_move = 3
         self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
