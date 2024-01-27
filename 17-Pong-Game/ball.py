b665409633c1d2ca7c4acf41db7248d8bf80d495
Streetprophet90 <hamoafo@gmail.com>
2024-01-27 14:52:21 +0000

    class ball set

---
+++ 17-Pong-Game/ball.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
