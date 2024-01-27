fbc60f0fb8e17bcd22c2afe460db2e30d30f57e6
Streetprophet90 <hamoafo@gmail.com>
2024-01-27 14:55:50 +0000

    bounce_x and reset position defined

--- 17-Pong-Game/ball.py
+++ 17-Pong-Game/ball.py
@@ -18,3 +18,12 @@ class Ball(Turtle):
 
     def bounce_y(self):
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
