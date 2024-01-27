555f0b85ba60ea1d6a5fa8ee44dd1e3c7b29658a
Streetprophet90 <hamoafo@gmail.com>
2024-01-27 14:21:31 +0000

    turns and random number defined

--- 16-Guessing-Number-Game/main.py
+++ 16-Guessing-Number-Game/main.py
@@ -11,3 +11,16 @@ logo = """
                       __/ |         __/ | 
                      |___/         |___/  
 """
+
+print(logo)
+print("Welcome to the Number Guessing Game!")
+
+# Generate a random number between 1 and 100 as the target.
+target_number = random.randint(1, 100)
+
+# Define different numbers of allowed guesses for each level.
+easy_turns = 10
+hard_turns = 5
+
+# Set the number of turns based on the selected level.
+turns_remaining = easy_turns  # Or hard_turns for hard mode.
