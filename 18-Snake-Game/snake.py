c07ac03d0305546e9d0a3aa53a772880e55793c9
Streetprophet90 <hamoafo@gmail.com>
2024-01-29 14:39:26 +0000

    create snake and add segment classes

--- 18-Snake-Game/snake.py
+++ 18-Snake-Game/snake.py
@@ -13,3 +13,14 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
