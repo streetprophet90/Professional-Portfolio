96dd4ea7d203158eb3e23dc7c0d5cb4b99ff0f45
Streetprophet90 <hamoafo@gmail.com>
2024-01-29 14:42:44 +0000

    extend and move fxn defined

--- 18-Snake-Game/snake.py
+++ 18-Snake-Game/snake.py
@@ -24,3 +24,14 @@ class Snake:
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+
+    def extend(self):
+        # add a new segment to the snake
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
