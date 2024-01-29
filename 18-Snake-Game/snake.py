322ac5cae7035ef1699efcb185b335ce97bcba39
Streetprophet90 <hamoafo@gmail.com>
2024-01-29 14:44:41 +0000

    up and down defined

--- 18-Snake-Game/snake.py
+++ 18-Snake-Game/snake.py
@@ -35,3 +35,11 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
