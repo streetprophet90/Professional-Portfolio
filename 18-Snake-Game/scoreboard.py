760bdbdd441e2e189770576d0f0bdb1d8e439929
Streetprophet90 <hamoafo@gmail.com>
2024-01-29 14:57:56 +0000

    initialise scoreboard

---
+++ 18-Snake-Game/scoreboard.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.hideturtle()
