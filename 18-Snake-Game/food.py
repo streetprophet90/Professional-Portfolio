d69fc739ebd1e47a19133f9b46ee28aaba7e5d11
Streetprophet90 <hamoafo@gmail.com>
2024-01-29 14:51:59 +0000

    refresh fxn defined

--- 18-Snake-Game/food.py
+++ 18-Snake-Game/food.py
@@ -11,3 +11,10 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
+        self.refresh()
+
+
+    def refresh(self):
+        random_x = random.randint(-280, 200)
+        random_y = random.randint(-280, 200)
+        self.goto(random_x, random_y)
