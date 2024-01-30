9492946de69f388eba12ead9196fdfa16ab52842
Streetprophet90 <hamoafo@gmail.com>
2024-01-30 17:36:19 +0000

    canvas created

--- 19-Pomodoro-GUI-Application/main.py
+++ 19-Pomodoro-GUI-Application/main.py
@@ -26,6 +26,11 @@ window.config(padx=100, pady=50, bg=YELLOW)
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
 
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="Tomato.jpg")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
 
 window.mainloop()
