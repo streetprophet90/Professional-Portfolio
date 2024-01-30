e86607c0ee11326939d0b6573a4c0f5ee53bc3e1
Streetprophet90 <hamoafo@gmail.com>
2024-01-30 18:09:20 +0000

    start button and reset button created

--- 19-Pomodoro-GUI-Application/main.py
+++ 19-Pomodoro-GUI-Application/main.py
@@ -77,6 +77,12 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(column=2, row=2)
+
 check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
