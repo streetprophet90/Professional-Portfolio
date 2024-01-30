08ad5aeaf277189b2e3ac194e53d4ff19634a885
Streetprophet90 <hamoafo@gmail.com>
2024-01-30 18:08:40 +0000

    start timer loop

--- 19-Pomodoro-GUI-Application/main.py
+++ 19-Pomodoro-GUI-Application/main.py
@@ -31,6 +31,16 @@ def start_timer():
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
 
