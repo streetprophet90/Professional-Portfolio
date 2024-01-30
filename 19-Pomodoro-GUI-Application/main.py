72a9e0fbdd6e3f6aa078f653428e99121995f986
Streetprophet90 <hamoafo@gmail.com>
2024-01-30 17:45:23 +0000

    check marks grid set

--- 19-Pomodoro-GUI-Application/main.py
+++ 19-Pomodoro-GUI-Application/main.py
@@ -32,5 +32,9 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+
 
 window.mainloop()
