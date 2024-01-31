83c9e2dbd3676f5d847d833da85a6bd47a37aacb
Streetprophet90 <hamoafo@gmail.com>
2024-01-31 14:15:02 +0000

    card canvas

--- 20-FLASH-CARD/main.py
+++ 20-FLASH-CARD/main.py
@@ -43,4 +43,7 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 flip_timer = window.after(3000, func=flip_card)
 
-
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
