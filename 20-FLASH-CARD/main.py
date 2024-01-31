3c53fd46d51bad92f579032b7dd53c5af3b17151
Streetprophet90 <hamoafo@gmail.com>
2024-01-31 14:12:58 +0000

    next card fxn defined

--- 20-FLASH-CARD/main.py
+++ 20-FLASH-CARD/main.py
@@ -14,4 +14,12 @@ except FileNotFoundError:
 else:
     to_learn = data.to_dict(orient="records")
 
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
