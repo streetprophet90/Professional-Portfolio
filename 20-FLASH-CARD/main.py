9382beba7a3f53b75ad37b30bcffe419af608724
Streetprophet90 <hamoafo@gmail.com>
2024-01-31 14:12:18 +0000

    try excep else

--- 20-FLASH-CARD/main.py
+++ 20-FLASH-CARD/main.py
@@ -6,4 +6,12 @@ BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
 to_learn = {}
 
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
