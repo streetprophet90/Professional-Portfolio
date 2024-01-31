8ee9f05ada4235fd4c035ff9892974760b8ca5a2
Streetprophet90 <hamoafo@gmail.com>
2024-01-31 14:15:29 +0000

    canvas grid

--- 20-FLASH-CARD/main.py
+++ 20-FLASH-CARD/main.py
@@ -47,3 +47,8 @@ canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
