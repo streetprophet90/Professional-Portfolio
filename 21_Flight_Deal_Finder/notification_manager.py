307d10a6e08591c493b85a34d6ec47390393b0b9
Streetprophet90 <hamoafo@gmail.com>
2024-02-01 16:59:08 +0000

    initialise

--- 21_Flight_Deal_Finder/notification_manager.py
+++ 21_Flight_Deal_Finder/notification_manager.py
@@ -5,4 +5,9 @@ TWILIO_AUTH_TOKEN = "YOUR AUTH TOKEN"
 TWILIO_VIRTUAL_NUMBER = "YOUR VIRTUAL NUMBER"
 TWILIO_VERIFIED_NUMBER = "YOUR NUMBER"
 
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
 
