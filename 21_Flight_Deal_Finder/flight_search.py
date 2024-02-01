cd134b82731359567ba531aca1fb813cd65de46a
Streetprophet90 <hamoafo@gmail.com>
2024-02-01 16:47:24 +0000

    get destination code

--- 21_Flight_Deal_Finder/flight_search.py
+++ 21_Flight_Deal_Finder/flight_search.py
@@ -4,4 +4,15 @@ from flight_data import FlightData
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 TEQUILA_API_KEY = "mtvEkBHIpIVx-ccQbKE3ktG1cFF3Aaha"
 
+class FlightSearch:
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
 
