f72e835dc1b728122183c09602cce72eb3cc0a8b
Streetprophet90 <hamoafo@gmail.com>
2024-02-01 16:44:22 +0000

    flight data created

---
+++ 21_Flight_Deal_Finder/flight_data.py
@@ -0,0 +1,6 @@
+class FlightData:
+
+    def __init__(self, price, origin_city, origin_airport, destination_city,
+                 destination_airport, out_date, return_date):
+        self.price = price
+        self.origin_city = origin_city
