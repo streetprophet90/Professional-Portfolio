87470f7b18e9c2413c3876dbc66240401d7b98f2
Streetprophet90 <hamoafo@gmail.com>
2024-02-01 17:02:29 +0000

    notification imported

--- 21_Flight_Deal_Finder/main.py
+++ 21_Flight_Deal_Finder/main.py
@@ -1,7 +1,12 @@
 from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+ORIGIN_CITY_IATA = "LON"
+
