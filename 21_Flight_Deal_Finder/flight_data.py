b67713c414dd8e5e40c227c69b3e7f1f27b990e7
Streetprophet90 <hamoafo@gmail.com>
2024-02-01 16:44:59 +0000

    flight data further defined

--- 21_Flight_Deal_Finder/flight_data.py
+++ 21_Flight_Deal_Finder/flight_data.py
@@ -4,3 +4,8 @@ class FlightData:
                  destination_airport, out_date, return_date):
         self.price = price
         self.origin_city = origin_city
+        self.origin_airport = origin_airport
+        self.destination_city = destination_city
+        self.destination_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
