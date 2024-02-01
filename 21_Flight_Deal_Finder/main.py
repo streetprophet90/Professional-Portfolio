b484ef649fe6610f61fe10610d6ce0fe4723c3a5
Streetprophet90 <hamoafo@gmail.com>
2024-02-01 16:53:22 +0000

    flight search imported

--- 21_Flight_Deal_Finder/main.py
+++ 21_Flight_Deal_Finder/main.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 from data_manager import DataManager
-
+from flight_search import FlightSearch
 
 data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
