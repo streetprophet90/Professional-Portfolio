36cbbf738ecf873c5c63ebcfeeef9f78d9829f09
Streetprophet90 <hamoafo@gmail.com>
2024-02-01 17:03:40 +0000

    for destination loop

--- 21_Flight_Deal_Finder/main.py
+++ 21_Flight_Deal_Finder/main.py
@@ -19,3 +19,12 @@ if sheet_data[0]["iataCode"] == "":
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+
+
