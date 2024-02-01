da44f8d16c7dbcfce122b422e7dfc79b45e6dee9
Streetprophet90 <hamoafo@gmail.com>
2024-02-01 16:41:34 +0000

    class DataManager set

--- 21_Flight_Deal_Finder/data_manager.py
+++ 21_Flight_Deal_Finder/data_manager.py
@@ -4,3 +4,17 @@ import requests
 SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/3c58721b1d082ba6eb4e152203aa0c2e/flightDeals/prices"
 
 
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
+        return self.destination_data
+
