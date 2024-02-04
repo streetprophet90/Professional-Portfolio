14f5feb1d6c1b5f8978bb5e802e162a74ffc2c15
Streetprophet90 <hamoafo@gmail.com>
2024-02-04 11:53:32 +0000

    process_coins fxn defined

--- 22-Coffee-Maker/money_machine.py
+++ 22-Coffee-Maker/money_machine.py
@@ -16,3 +16,10 @@ class MoneyMachine:
         """Prints the current profit"""
         print(f"Money: {self.CURRENCY}{self.profit}")
 
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+        return self.money_received
+
