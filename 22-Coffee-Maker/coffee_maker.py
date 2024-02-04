2cb6babc033da563d7e7c88e2d8d384daeebe251
Streetprophet90 <hamoafo@gmail.com>
2024-02-04 11:46:10 +0000

    class CoffeeMaker init

---
+++ 22-Coffee-Maker/coffee_maker.py
@@ -0,0 +1,9 @@
+class CoffeeMaker:
+    """Models the machine that makes the coffee"""
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+
