e3621c010da7365e7266996376c6949a035c881b
Streetprophet90 <hamoafo@gmail.com>
2024-02-04 11:40:53 +0000

    class Menu

--- 22-Coffee-Maker/menu.py
+++ 22-Coffee-Maker/menu.py
@@ -10,3 +10,13 @@ class MenuItem:
         }
 
 
+class Menu:
+    """Models the Menu with drinks."""
+    def __init__(self):
+        self.menu = [
+            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+            MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
+            MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
+        ]
+
+
