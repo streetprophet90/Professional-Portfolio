d8a13a1633db33f9ab1efe5836ccd63565a9e444
Streetprophet90 <hamoafo@gmail.com>
2024-02-05 14:03:04 +0000

    canvas

--- 23-PASSWORD-MANAGER/main.py
+++ 23-PASSWORD-MANAGER/main.py
@@ -12,6 +12,11 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
 
 
 window.mainloop()
