753aa5d7ecd41242f6dca7b67595daa60263b65f
Streetprophet90 <hamoafo@gmail.com>
2024-02-05 14:02:35 +0000

    UI setup

---
+++ 23-PASSWORD-MANAGER/main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
+
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+
+
+window.mainloop()
