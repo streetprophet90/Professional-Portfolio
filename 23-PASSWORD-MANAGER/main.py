7b5b45d0ba6ed135a93619fb70f465e62a2b297a
Streetprophet90 <hamoafo@gmail.com>
2024-02-05 14:04:54 +0000

    Entries

--- 23-PASSWORD-MANAGER/main.py
+++ 23-PASSWORD-MANAGER/main.py
@@ -25,6 +25,16 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
+#Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "amoafo.alive@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
 
 
 window.mainloop()
