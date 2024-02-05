cc1aab6d4669f0212c19f56cf099a891f02ef0b7
Streetprophet90 <hamoafo@gmail.com>
2024-02-05 14:13:52 +0000

    logo error solved

--- 23-PASSWORD-MANAGER/main.py
+++ 23-PASSWORD-MANAGER/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+
 import pyperclip
 
 
@@ -50,8 +51,8 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
+# logo_img = PhotoImage(file="logo.jpg")
+# canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 #Labels
