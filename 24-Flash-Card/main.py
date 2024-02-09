dbafd8810e0fcc77e97635e37c7837a8da19f004
Streetprophet90 <hamoafo@gmail.com>
2024-02-09 13:20:27 +0000

    window setup

---
+++ 24-Flash-Card/main.py
@@ -0,0 +1,26 @@
+from tkinter import *
+import pandas
+import random
+
+
+
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+
+
+window.mainloop()
