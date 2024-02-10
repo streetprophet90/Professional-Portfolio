5aa5b2b5e95288a1b769d2c15a57af7e47c28ffb
Streetprophet90 <hamoafo@gmail.com>
2024-02-10 19:15:07 +0000

    error fixed

--- 25-Send-Email/main.py
+++ 25-Send-Email/main.py
@@ -1,16 +1,3 @@
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
 from datetime import datetime
 import pandas
 import random
@@ -27,7 +14,7 @@ birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, dat
 if today_tuple in birthdays_dict: # 2. Check if today matches a birthday in the birthdays.csv
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
- with open(file_path) as letter_file:
+    with open(file_path) as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
