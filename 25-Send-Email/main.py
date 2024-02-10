b5ec942badd8d0dfb3db947da4f619196dfe770e
Streetprophet90 <hamoafo@gmail.com>
2024-02-10 19:08:48 +0000

    if statements

--- 25-Send-Email/main.py
+++ 25-Send-Email/main.py
@@ -24,3 +24,6 @@ today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict: # 2. Check if today matches a birthday in the birthdays.csv
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
