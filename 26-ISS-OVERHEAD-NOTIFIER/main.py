bc04e904ce7cb772a20913f2b3d673b326cdfed6
Streetprophet90 <hamoafo@gmail.com>
2024-02-13 23:18:12 +0000

    constants

---
+++ 26-ISS-OVERHEAD-NOTIFIER/main.py
@@ -0,0 +1,10 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_EMAIL = ""
+MY_PASSWORD = ""
+MY_LAT = 6.695070 # Your latitude
+MY_LONG = -1.615800 # Your longitude
+
