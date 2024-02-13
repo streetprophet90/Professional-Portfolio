e61e0d99eec711b86385a4cc293a5f600d23d22e
Streetprophet90 <hamoafo@gmail.com>
2024-02-13 23:18:44 +0000

    is_iss_overhead fxn defined

--- 26-ISS-OVERHEAD-NOTIFIER/main.py
+++ 26-ISS-OVERHEAD-NOTIFIER/main.py
@@ -8,3 +8,11 @@ MY_PASSWORD = ""
 MY_LAT = 6.695070 # Your latitude
 MY_LONG = -1.615800 # Your longitude
 
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
