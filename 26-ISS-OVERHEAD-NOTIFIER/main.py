998bb3896be24bd748004998fc97bfe9ee21b5a2
Streetprophet90 <hamoafo@gmail.com>
2024-02-13 23:20:06 +0000

    json

--- 26-ISS-OVERHEAD-NOTIFIER/main.py
+++ 26-ISS-OVERHEAD-NOTIFIER/main.py
@@ -28,3 +28,14 @@ def is_night():
         "formatted": 0,
     }
 
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
